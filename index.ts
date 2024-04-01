ac188752474ff4202b9c06f8d9b433b612559cfb
Madiha-Ayaz <161247604+Madiha-Ayaz@users.noreply.github.com>
2024-04-01 12:06:58 -0700

    my cli- guessing game push up on github

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()*5+1);
+console.log( "welcome " );
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a number between 1 to 5):",
+    },
+]);
+console.log(answers);
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congratulations! you guess a right number .");
+}else {
+    console.log("you guess a wrong number");
+}
